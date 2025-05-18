a5deeb10ccf5a64174d95f6461e185f83c0bfbf5
cornshakes <111193774+cornshakes@users.noreply.github.com>
2025-05-18 15:15:57 +0200

    init package

---
+++ README.md
@@ -0,0 +1,5 @@
+LocalNorthwindEF is a self contained Northwind db to practice Entity Framework skills.
+
+It uses a pre-generated northwind.db file from this project: https://github.com/jpwhite3/northwind-SQLite3
+
+EF Core Power Tools were used to generate all the model & context classes.
---
+++ Test/NorthwindContextTest.cs
@@ -0,0 +1,21 @@
+﻿using LocalNorthwindEF;
+
+namespace Test
+{
+    public class NorthwindContextTest
+    {
+        [Fact]
+        public void SimpleNorthwindContextWorks()
+        {
+            var context = new SimpleNorthwindContext();
+            Assert.Equal(77, context.Products.Count());
+        }
+
+        [Fact]
+        public void NorthwindContextWithViewsWorks()
+        {
+            var context = new NorthwindContextWithViews();
+            Assert.Equal(69, context.AlphabeticalListOfProducts.Count());
+        }
+    }
+}
